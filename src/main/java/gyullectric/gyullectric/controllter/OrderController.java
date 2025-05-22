0188eb235a4be7e30d222f6ab21e74b859dbcbe9
Krantz <msboy8548@gmail.com>
2025-05-22 17:55:04 +0900

    0522 17:54 commit

--- src/main/java/gyullectric/gyullectric/controllter/OrderController.java
+++ src/main/java/gyullectric/gyullectric/controllter/OrderController.java
@@ -1,6 +1,8 @@
 package gyullectric.gyullectric.controllter;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,7 +21,12 @@ public class OrderController {
     }
 
     @GetMapping("/list")
-    public String getOrderList () {
+    public String getOrderList (Model model, HttpSession session) {
+        if (session.getAttribute("user") == null) {
+            return "redirect:/login";
+        }
+        // 컨트롤러 서비스 연결 필요
+        // model.addAttribute("orderList", orderList);
         return "product/orderList";
     }
 
