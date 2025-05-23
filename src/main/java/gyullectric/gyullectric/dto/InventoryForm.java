d4dee587383f9a1c0c591a150c53181ef68aff62
Krantz <msboy8548@gmail.com>
2025-05-23 16:22:48 +0900

    0523 dto 정의

--- src/main/java/gyullectric/gyullectric/dto/InventoryForm.java
+++ src/main/java/gyullectric/gyullectric/dto/InventoryForm.java
@@ -1,4 +1,28 @@
 package gyullectric.gyullectric.dto;
 
+import gyullectric.gyullectric.domain.PartName;
+import gyullectric.gyullectric.domain.Supplier;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class InventoryForm {
+    private Long id; // 수정 시 필요
+
+    @NotNull(message = "자재명은 필수입니다")
+    private PartName partName;
+
+    @NotNull(message = "공급업체는 필수입니다")
+    private Supplier supplier;
+
+    @Min(value = 0, message = "수량은 1 이상이어야 합니다")
+    private int quantity;
+
+    private LocalDateTime orderedAt; // 발주 일시, 선택 가능 (기본 현재시간)
 }
